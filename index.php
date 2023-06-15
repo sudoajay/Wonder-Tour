073266b4ee20048dbb8fd075630a0b50ba32cff5
SudoAjay <sudoajay@gmail.com>
2023-06-15 15:12:45 +0530

    done

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ include('include/header.php')
 <section class="section section-sm section-first bg-default text-md-left">
     <div class="container">
         <div class="row row-50 align-items-center justify-content-center justify-content-xl-between">
-            <div class="col-lg-6 text-center wow fadeInUp"><img src="images/270/andaman2.jpg=" width="556" height="382" />
+            <div class="col-lg-6 text-center wow fadeInUp"><img src="images/270/andamanscuba.jpg" width="556" height="382" />
             </div>
             <div class="col-lg-6 wow fadeInRight" data-wow-delay=".1s">
                 <div class="box-width-lg-470">
